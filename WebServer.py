d0f3c9a3107656e1016a8683070d51f5355de723
Anna-Livia <annalivia.cardin@gmail.com>
2017-04-10 17:09:52 +0200

    Deals with html pages otherwise 404 error

--- WebServer.py
+++ WebServer.py
@@ -1,51 +1,75 @@
-#http://www.aosabook.org/en/500L/a-simple-web-server.html
+import sys, os, BaseHTTPServer
 
-import BaseHTTPServer
+#-------------------------------------------------------------------------------
+
+class ServerException(Exception):
+    '''For internal error reporting.'''
+    pass
+
+#-------------------------------------------------------------------------------
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    '''Handle HTTP requests by returning a fixed 'page'.'''
+    '''
+    If the requested path maps to a file, that file is served.
+    If anything goes wrong, an error page is constructed.
+    '''
 
-    # Page to send back.
-    Page = '''\
+    # How to display an error.
+    Error_Page = """\
         <html>
         <body>
-        <table>
-        <tr>  <td>Header</td>         <td>Value</td>          </tr>
-        <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
-        <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
-        <tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
-        <tr>  <td>Command</td>        <td>{command}</td>      </tr>
-        <tr>  <td>Path</td>           <td>{path}</td>         </tr>
-        </table>
+        <h1>Error accessing {path}</h1>
+        <p>{msg}</p>
         </body>
         </html>
-        '''
+        """
 
-    # Handle a GET request.
+    # Classify and handle request.
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        try:
 
-    def create_page(self):
-        values = {
-            'date_time': self.date_time_string(),
-            'client_host': self.client_address[0],
-            'client_port': self.client_address[1],
-            'command': self.command,
-            'path': self.path
-        }
-        page = self.Page.format(**values)
-        return page
+            # Figure out what exactly is being requested.
+            full_path = os.getcwd() + self.path
 
+            # It doesn't exist...
+            if not os.path.exists(full_path):
+                raise ServerException("'{0}' not found".format(self.path))
 
-    def send_page(self, page):
+            # ...it's a file...
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+
+            # ...it's something we don't handle.
+            else:
+                raise ServerException("Unknown object '{0}'".format(self.path))
+
+        # Handle errors.
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+    # Handle unknown objects.
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_content(content)
+
+    # Send actual content.
+    def send_content(self, content):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(len(page)))
+        self.send_header("Content-Length", str(len(content)))
         self.end_headers()
-        self.wfile.write(page)
+        self.wfile.write(content)
 
-#----------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     serverAddress = ('', 8080)
